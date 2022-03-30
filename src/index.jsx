94581c464036c0c1b4da797662312838865475e1
andrewcoba <andrewcoba@gmail.com>
2022-03-30 17:27:36 -0600

    removed main from package.json

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
 
-// Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
     return (
