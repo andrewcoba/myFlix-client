d4d38936d0744c32369f1380ac7b5618d4807e85
andrewcoba <andrewcoba@gmail.com>
2022-04-03 17:09:17 -0600

    React component implementation

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,13 +1,42 @@
 import React from 'react';
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
 
 class MainView extends React.Component {
 
+}
+export class MainView extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      movies: [
+        { _id: 1, Title: 'Inception', Description: 'description', ImagePath: '...' },
+        { _id: 2, Title: 'The Shawshank Redemption', Decription: 'description', ImagePath: '...' },
+        { _id: 3, Title: 'Gladiator', Description: 'description 3', ImagePath: '...' }
+      ]
+    }
+  }
+
+  setSelectedMovie(newSelectedMovie) {
+    this.setState({
+      selectedMovie: newSelectedMovie
+    });
+  }
+
   render() {
+    const { movies } = this.state;
+
+    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+
     return (
       <div className="main-view">
-        <div>Inception</div>
-        <div>The Shawshank Redemption</div>
-        <div>Gladiator</div>
+        {selectedMovie
+          ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+          : movies.map(movie => (
+            <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
+          ))
+        }
       </div>
     );
   }
